a5fa7055a360107106abf7bfa45b1e94ad017303
Jan Kopański <jan@kopanski.eu>
2020-03-24 00:03:15 +0100

    pybatsim repo download on run

--- run_pybatsim_git.sh
+++ run_pybatsim_git.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # ./run_pybatsim_git.sh schedulers/burstBufferScheduler.py
+[ ! -d "pybatsim" ] && git clone https://gitlab.inria.fr/batsim/pybatsim.git
 python pybatsim/launcher.py -v warn "$1"
