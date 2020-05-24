6377eff53942c7096c29b169a1aa6bf8e75e41df
Jan Kopański <jan@kopanski.eu>
2020-05-24 21:32:34 +0200

    Backfill scheduler parameter

--- burstbuffer/backfillAllocOnlyScheduler.py
+++ burstbuffer/backfillAllocOnlyScheduler.py
@@ -2,5 +2,9 @@ from burstbuffer.alloc_only import AllocOnlyScheduler
 
 
 class BackfillAllocOnlyScheduler(AllocOnlyScheduler):
+    def __init__(self, options):
+        super().__init__(options)
+        self.backfilling_reservation_depth = options['backfill']
+
     def schedule(self):
-        self._backfill_schedule()
+        self._backfill_schedule(backfilling_reservation_depth=self.backfilling_reservation_depth)
