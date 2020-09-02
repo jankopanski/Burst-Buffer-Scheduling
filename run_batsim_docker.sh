3040a7ddfbfd087590285a6744d7c461b52ab573
Jan Kopański <jan@kopanski.eu>
2020-09-02 17:54:21 +0200

    Update running scripts to Batsim version 4.0.0

--- run_batsim_docker.sh
+++ run_batsim_docker.sh
@@ -7,6 +7,9 @@
 # ./run_batsim_docker.sh -p platforms/dragonfly.xml -w workloads/generated_cluster30_jobs100.json
 # -r node_0,node_6,node_12,node_18,node_24,node_30:storage
 
+#./run_batsim_docker.sh -p platforms/dragonfly96.xml -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json
+#-r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage -e output/out
+
 #platform=$1
 #workload=$2
 
@@ -17,7 +20,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     --net host \
     -u $user_id:$group_id \
     -v $PWD:/data \
-    oarteam/batsim:3.1.0 \
+    oarteam/batsim:4.0.0 \
     -q \
     "$@"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -25,7 +28,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     --net host \
     -u $user_id:$group_id \
     -v $PWD:/data \
-    oarteam/batsim:3.1.0 \
+    oarteam/batsim:4.0.0 \
     -s tcp://host.docker.internal:28000 \
     -q \
     "$@"
--- run_pybatsim_git.sh
+++ run_pybatsim_git.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 [ ! -d "pybatsim" ] && git clone https://gitlab.inria.fr/batsim/pybatsim.git
-# ./run_pybatsim_git.sh schedulers/fcfsAllocOnlyScheduler.py platforms/dragonfly.yaml
+
+# ./run_pybatsim_git.sh burstbuffer/fcfsAllocOnlyScheduler.py platforms/dragonfly.yaml
+
 #scheduler_config="{\"platform\": \"$2\", \"progress_bar\": false}"
 #echo "$scheduler_config"
 #python pybatsim/launcher.py -v warn "$1" -o "$scheduler_config" 2> /dev/null
-#
-# ./run_pybatsim_git.sh schedulers/fcfsAllocOnlyScheduler.py scheduler_options.json
+
+# ./run_pybatsim_git.sh burstbuffer/fcfsAllocOnlyScheduler.py scheduler_options.json
 python pybatsim/launcher.py -v warn "$1" -O "$2"
