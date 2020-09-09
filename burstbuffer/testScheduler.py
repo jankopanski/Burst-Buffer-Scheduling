fd2fb4b960de686261b92c4d26124b8ea14939c0
Jan Kopański <jan@kopanski.eu>
2020-09-09 18:50:05 +0200

    Test scheduler for I/O simulation

---
+++ burstbuffer/testScheduler.py
@@ -0,0 +1,134 @@
+from batsim.sched.scheduler import Scheduler
+from batsim.sched.profiles import Profiles
+from batsim.sched.job import Job
+from batsim.sched.alloc import Allocation
+from procset import ProcSet
+
+from .storage_resource import StorageResource
+
+
+class StorageAllocation(Allocation):
+    def __len__(self):
+        return len(self.special_resources)
+
+
+class TestScheduler(Scheduler):
+
+    def __init__(self, options):
+        super().__init__(options=options)
+        # self._logger._logger.setLevel('WARNING')
+        self._event_logger._logger.setLevel('WARNING')
+        self.num_submitted = 0
+
+    def on_init(self):
+        for storage_resource in self.machines['storage']:
+            if storage_resource['name'] == 'pfs':
+                self._pfs = StorageResource(
+                    scheduler=self,
+                    id=storage_resource["id"],
+                    name=storage_resource["name"],
+                    resources_list=self.resources
+                )
+            else:
+                self._resources.add(StorageResource(
+                    scheduler=self,
+                    id=storage_resource["id"],
+                    name=storage_resource["name"],
+                    resources_list=self.resources
+                ))
+
+    def on_job_submission(self, job):
+        if not job.is_dynamic_job:
+            requested_resources = job.requested_resources
+            walltime = 3
+            # profile1 = Profiles.ParallelHomogeneous(cpu=10**9, com=0, bb=0)
+            # job.submit_sub_job(requested_resources, walltime, profile1)
+            profile1 = Profiles.DataStaging(size=10 ** 8)
+            job.submit_sub_job(2, walltime, profile1)
+            profile2 = Profiles.ParallelHomogeneous(cpu=2*10**9, com=0, bb=0)
+            job.submit_sub_job(requested_resources, walltime, profile2)
+            self.num_submitted += 2
+
+    def on_job_completion(self, job):
+        next_job = job.parent_job.sub_jobs.runnable.first
+        if next_job:
+            new_allocation = Allocation(
+                start_time=self.time,
+                walltime=job.allocation.walltime - (self.time - job.allocation.start_time),
+                resources=job.parent_job.assigned_compute_resources
+            )
+            next_job.schedule(new_allocation)
+        print('completed ', job.id)
+        # job.allocation.remove_all_resources()
+        # job.allocation.free()
+        # job.free()
+        # job.parent_job.reject()
+        pass
+
+    def on_end(self):
+        pass
+
+    def schedule(self):
+        if self._batsim.no_more_static_jobs:
+            print('end of static jobs')
+            self.notify_registration_finished()
+        self.high_level_sub_job()
+        # self.high_level_new_dynamic_job_registration()
+
+    def high_level_sub_job(self):
+        for job in self.jobs.static_job.runnable:
+            for sub_job in job.sub_jobs:
+                assigned_compute_resources = \
+                    self.resources.compute.find_sufficient_resources_for_job(job)
+                assert assigned_compute_resources
+                job.assigned_compute_resources = assigned_compute_resources
+                self.schedule_data_staging(
+                    sub_job, self._pfs, self.resources.special.first, job.requested_time)
+                job.reject()
+                break
+                # job.change_state(Job.State.RUNNING)
+
+    def high_level_new_dynamic_job_registration(self):
+        # Requires --enable-dynamic-jobs --acknowledge-dynamic-jobs
+        jobs = self.jobs.runnable
+        for job in jobs:
+            if job.id == 'w0!0':
+                new_profile = Profiles.ParallelHomogeneous(cpu=10 ** 9, com=0, bb=0)
+                self.register_dynamic_job(
+                    1,
+                    10,
+                    new_profile,
+                )
+                self.notify_registration_finished()
+            assigned_compute_resources = self.resources.compute.find_sufficient_resources_for_job(
+                job)
+            job.schedule(assigned_compute_resources)
+            print('scheduled ', job.id)
+
+        for dynamic_job in self.dynamic_workload.acknowledged_jobs:
+            print('DYNAMIC ', dynamic_job.id)
+        #     assigned_compute_resources = self.resources.compute.find_sufficient_resources_for_job(
+        #         dynamic_job)
+        #     dynamic_job.schedule(assigned_compute_resources)
+
+    def schedule_data_staging(
+            self,
+            job: Job,
+            source: StorageResource,
+            destination: StorageResource,
+            walltime=None
+    ):
+        allocation = StorageAllocation(
+            start_time=self.time,
+            walltime=walltime if walltime else -1,
+            resources=[source, destination],
+            job=job
+        )
+        allocation.allocate_all(self)
+        job._allocation = allocation
+        job._batsim_job.allocation = ProcSet(source.id, destination.id)
+        job._batsim_job.storage_mapping = {
+            'source': source.id,
+            'destination': destination.id
+        }
+        self._batsim.execute_job(job._batsim_job)
--- run_pybatsim_git.sh
+++ run_pybatsim_git.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-#[ ! -d "pybatsim" ] && git clone https://gitlab.inria.fr/batsim/pybatsim.git
+[ ! -d "pybatsim" ] && git submodule update --init
+
+set -x
 
 # ./run_pybatsim_git.sh burstbuffer/fcfsAllocOnlyScheduler.py platforms/dragonfly.yaml
-
 #scheduler_config="{\"platform\": \"$2\", \"progress_bar\": false}"
 #echo "$scheduler_config"
 #python pybatsim/launcher.py -v warn "$1" -o "$scheduler_config" 2> /dev/null
