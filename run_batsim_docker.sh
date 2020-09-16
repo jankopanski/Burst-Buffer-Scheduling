5ff531d99b105252224c49d3df79ba459232ac23
Jan Kopański <jan@kopanski.eu>
2020-09-16 22:29:34 +0200

    Update running scripts

--- run_batsim_docker.sh
+++ run_batsim_docker.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
-# ./run_batsim_docker.sh platforms/two_nodes_bb_pfs.xml workloads/example_compute_with_bb.json
 
-# ./run_batsim_docker.sh --enable-dynamic-jobs -p platforms/two_nodes_bb_pfs.xml
-# -w workloads/example_compute_with_bb.json
+# AllocOnlyScheduler
+#./run_batsim_docker.sh -p platforms/dragonfly96.xml -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage -e output/out
 
-# ./run_batsim_docker.sh -p platforms/dragonfly.xml -w workloads/generated_cluster30_jobs100.json
-# -r node_0,node_6,node_12,node_18,node_24,node_30:storage
-
-#./run_batsim_docker.sh -p platforms/dragonfly96.xml -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json
-#-r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage -e output/out
+# IOAwareScheduler
+#./run_batsim_docker.sh -p platforms/dragonfly96.xml -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage -e output/out --enable-dynamic-jobs --enable-profile-reuse
 
 #platform=$1
 #workload=$2
--- run_pybatsim_git.sh
+++ run_pybatsim_git.sh
@@ -3,10 +3,16 @@
 
 set -x
 
-# ./run_pybatsim_git.sh burstbuffer/fcfsAllocOnlyScheduler.py platforms/dragonfly.yaml
+# Run with arguments defined inside the script
+#./run_pybatsim_git.sh burstbuffer/schedAllocOnly.py platforms/dragonfly.yaml
 #scheduler_config="{\"platform\": \"$2\", \"progress_bar\": false}"
 #echo "$scheduler_config"
 #python pybatsim/launcher.py -v warn "$1" -o "$scheduler_config" 2> /dev/null
 
-# ./run_pybatsim_git.sh burstbuffer/fcfsAllocOnlyScheduler.py scheduler_options.json
+# AllocOnlyScheduler
+#./run_pybatsim_git.sh burstbuffer/schedAllocOnly.py scheduler_options.json
+
+# IOAwareScheduler
+#./run_pybatsim_git.sh burstbuffer/schedIOAware.py scheduler_options.json
+
 python pybatsim/launcher.py -v warn "$1" -O "$2"
