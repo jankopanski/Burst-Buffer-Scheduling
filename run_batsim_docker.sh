bd0f4924202667881aef79922f5b5209c4271ace
Jan Kopański <jan@kopanski.eu>
2020-10-01 15:20:01 +0200

    Hardcode platform

--- run_batsim_docker.sh
+++ run_batsim_docker.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # AllocOnlyScheduler
-#./run_batsim_docker.sh -p platforms/dragonfly96.xml -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage -e output/out
+#./run_batsim_docker.sh -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json -e output/out
 
 # IOAwareScheduler
-#./run_batsim_docker.sh -p platforms/dragonfly96.xml -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage -e output/out --enable-dynamic-jobs --enable-profile-reuse
+#./run_batsim_docker.sh -w workloads/KTH-SP2-1996-2.1-cln-1000-1.json -e output/out --enable-dynamic-jobs --enable-profile-reuse
 
 #platform=$1
 #workload=$2
@@ -18,6 +18,8 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     -v $PWD:/data \
     oarteam/batsim:4.0.0 \
     -s tcp://127.0.0.1:28000 \
+    -p platforms/dragonfly96.xml \
+    -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage \
     -q \
     "$@"
 elif [[ "$OSTYPE" == "darwin"* ]]; then
@@ -27,6 +29,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     -v $PWD:/data \
     oarteam/batsim:4.0.0 \
     -s tcp://host.docker.internal:28000 \
+    -p platforms/dragonfly96.xml \
+    -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage \
     -q \
     "$@"
 else
