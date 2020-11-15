206463bd0edad05e19a63ba01493763bc18fbd25
Jan Kopański <jan@kopanski.eu>
2020-11-15 12:54:27 +0100

    Create runner for split workload

--- run_batsim_docker.sh
+++ run_batsim_docker.sh
@@ -6,8 +6,7 @@
 # IOAwareScheduler
 #./run_batsim_docker.sh -w workloads/KTH-SP2-1996-2.1-cln-io-aware.json -e output/out --enable-dynamic-jobs --enable-profile-reuse
 
-#platform=$1
-#workload=$2
+[ -z "$BATSIM_PORT" ] && BATSIM_PORT=28000
 
 user_id=$(id -u)
 group_id=$(id -g)
@@ -18,7 +17,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     -v $PWD:/data \
     -m 9G \
     oarteam/batsim:4.0.0 \
-    -s tcp://127.0.0.1:28000 \
+    -s tcp://127.0.0.1:$BATSIM_PORT \
     -p platforms/dragonfly96.xml \
     -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage \
     -q \
@@ -29,7 +28,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
     -u $user_id:$group_id \
     -v $PWD:/data \
     oarteam/batsim:4.0.0 \
-    -s tcp://host.docker.internal:28000 \
+    -s tcp://host.docker.internal:$BATSIM_PORT \
     -p platforms/dragonfly96.xml \
     -r node_0,node_9,node_18,node_27,node_36,node_45,node_54,node_63,node_72,node_81,node_90,node_99:storage \
     -q \
