cff27e189b1dfb3849c5c6a2194bfe081d3e8263
Jan Kopański <jan@kopanski.eu>
2020-11-15 12:53:05 +0100

    Terminate on error

--- setup_simulator_env.sh
+++ setup_simulator_env.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -e  # Terminate script on error
+set -x  # Print commands
+
 wget https://downloads.python.org/pypy/pypy3.7-v7.3.2-linux64.tar.bz2
 tar xf pypy3.7-v7.3.2-linux64.tar.bz2
 virtualenv -p pypy3.7-v7.3.2-linux64/bin/pypy sim
