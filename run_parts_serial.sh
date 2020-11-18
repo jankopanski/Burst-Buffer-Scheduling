d36a83afe3bd8aa1009d073ec79763a166a5ff91
Jan Kopański <jan@kopanski.eu>
2020-11-18 20:09:25 +0100

    Add serial version of run_parts script

---
+++ run_parts_serial.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# ./run_parts.sh io fcfs
+
+MODEL=$1  # io/alloc
+CONFIG=$2  # scheduler options file
+
+PARTS=16
+FIRST_PORT=28005
+
+source sim/bin/activate
+
+for (( i=0; i<$PARTS; i++ )); do
+  export BATSIM_PORT=$((FIRST_PORT+i))
+  echo $BATSIM_PORT
+  echo ${CONFIG}_part-$i
+  if [ $MODEL == "io" ]; then
+    ./run_batsim_docker.sh -w workloads/KTH-split/io-aware/part-$i.json -e output/KTH-split/io-aware/${CONFIG}_part-$i --enable-dynamic-jobs --enable-profile-reuse &
+    python -O pybatsim/launcher.py -v warn -t 60000 burstbuffer/schedIOAware.py -O configs/$CONFIG.json -s tcp://127.0.0.1:$BATSIM_PORT
+  else
+    ./run_batsim_docker.sh -w workloads/KTH-split/alloc-only/part-$i.json -e output/KTH-split/alloc-only/${CONFIG}_part-$i  &
+    python -O pybatsim/launcher.py -v warn -t 60000 burstbuffer/schedAllocOnly.py -O configs/$CONFIG.json -s tcp://127.0.0.1:$BATSIM_PORT
+  fi
+done
